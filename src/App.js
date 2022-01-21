7c12b02cce08ba3e306e6e474e4f8e23e7c3f339
Ninad Sangli <ninad.sangli@komprise.com>
2022-01-21 15:33:17 +0530

    Sorted table working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Table from "./Components/Table/Table"
 
+
+class App extends Component {
+  state = {
+    stateData: []
+  }
+  componentDidMount(){
+    fetch("https://www.mohfw.gov.in/data/datanew.json")
+      .then(response => response.json())
+      .then(states => this.setState({stateData: states}));    
+  }
+
+  render(){
+    return (
+      <div className="App">
+        {
+          <div className="table-container">
+            {(this.state.stateData.length) ?
+              <Table states={this.state.stateData} rowKey={"state_name"}/> : null}
+          </div>
+
+        }
+      </div>
+    );
+  }  
+}
+ 
 export default App;
---
+++ src/Components/Table/Table.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { useTable, useSortBy } from 'react-table';
+import { Columns } from './columns';
+import './Table.css'
+
+const Table = ({ states }) => {
+    const data = React.useMemo(() => states, [states]);
+    const columns = React.useMemo(() => Columns, []);
+    
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        prepareRow,
+      } = useTable({ columns, data }, useSortBy)
+
+    return (
+    <table {...getTableProps()}>
+        <thead>
+            {
+                headerGroups.map(headerGroup => (
+                    <tr {...headerGroup.getHeaderGroupProps()} key={Math.random()}>
+                        {
+                            headerGroup.headers.map(column => (
+                                <th {...column.getHeaderProps(column.getSortByToggleProps())} key={Math.random()}>
+                                    {column.render('Header')}
+                                    <span>
+                                        {column.isSorted ? 
+                                            (column.isSortedDesc ? '⬇' : '⬆' )
+                                            : ''}
+                                    </span>
+                                </th>
+                            ))
+                        }
+                    </tr>
+                ))
+            }
+        </thead>
+        <tbody {...getTableBodyProps()}>
+            {
+                rows.map(row => {
+                    prepareRow(row);
+                    return (
+                        <tr {...row.GetRowProps} key={Math.random()}>
+                            {
+                                row.cells.map(cell => (
+                                    <td {...cell.getCellProps()}>
+                                        {cell.render('Cell')}
+                                    </td>
+                                ))
+                            }
+                        </tr>
+                    )
+                })
+            }
+        </tbody>
+    </table>
+    )
+   
+    }
+
+export default Table;
